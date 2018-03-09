8f81b66aeb1cd0a4c011a0d05cfbd1fafc1baafe
Aksimka <36276085+Aksimka@users.noreply.github.com>
2018-03-09 22:44:12 +0300

    add animation

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.header-content__button').hover(function(){
+        $(this).animate({box-shadow: 0px 0px 58px 10px rgba(0,0,0,0.75);})
+    },function(){})
+})
